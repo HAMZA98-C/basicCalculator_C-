002b43de2bfd90cc31178ceaa9151312960e47a5
Abdul Moiz <abdulmoizzzz72@gmail.com>
2025-08-03 02:54:19 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     double num1, num2;
     char operation;
-
+//Basic Calculator
     cout << "Basic Calculator in C++" << endl;
     cout << "Enter first number: ";
     cin >> num1;
@@ -37,3 +37,4 @@ int main() {
 
     return 0;
 }
+
